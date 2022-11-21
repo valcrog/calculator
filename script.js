f27234f32bebff29ce525275212b8fe67bf1082f
Valentin Crogiez <valentin.crogiez@gmail.com>
2022-11-21 17:56:30 +0100

    Created operate function

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ function multiply(a, b) {
 function divide(a, b) {
     return a/b;
 }
+
+function operate(operator, a, b) {
+    if (operator == "+") {
+        return add(a, b)
+    } else if (operator == "-") {
+        return substract(a, b)
+    } else if (operator == "*") {
+        return multiply(a, b)
+    } else if (operator == "/") {
+        return divide(a, b)
+    }
+}
+
