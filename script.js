e44fde41aaeda71844a2fd210f56a884f09d3219
valcrog <crogiez.valentin@gmail.com>
2023-01-12 20:37:59 +0100

    Created inputOperand function with buttons

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let displayValue = '0';
+let firstOperand = null;
+let secondOperand = null;
+let firstOperator = null;
+let secondOperator = null;
+let result = null;
+const buttons = document.querySelectorAll('button');
+
 function add(a, b) {
     return a+b;
 }
@@ -9,6 +17,47 @@ function multiply(a, b) {
 }
 function divide(a, b) {
     return a/b;
+}
+
+function updateDisplay() {
+    const display = document.getElementById('display');
+    console.log(display);
+    display.innerText = displayValue;
+    if (displayValue.length > 15) {
+        display.innerText = displayValue.substring(0, 15);
+    }
+} 
+
+updateDisplay();
+
+function clickButton() {
+    for (let i=0; i<buttons.length; i++) {
+        buttons[i].addEventListener('click', function() {
+            classes = buttons[i].classList;
+            if (classes.contains('digit')) {
+                inputOperand(buttons[i].value);
+                updateDisplay();
+            }
+        })
+    }
+}
+
+clickButton();
+
+function inputOperand(value) {
+    if (!firstOperator) {
+        if (displayValue === '0' || displayValue === firstOperand) {
+            displayValue = value
+        } else {
+            displayValue += value
+        }
+    } else {
+        if (displayValue === firstOperand) {
+            displayValue = value
+        } else {
+            displayValue += value
+        }
+    }
 }
 
 function operate(operator, a, b) {
